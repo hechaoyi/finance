af4de9942330e71309b82e10c47f1cb44c1c1598
和超逸 <chaoyi.he@qunar.com>
2016-08-18 11:04:44 +0800

    hello world

---
+++ src/main/java/com/hedatou/finance/Application.java
@@ -0,0 +1,17 @@
+package com.hedatou.finance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author hechaoyi@me.com
+ * @since 2016年8月18日 上午10:42:41
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
